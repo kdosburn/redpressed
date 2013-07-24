1eb0178c4011f244cfae1361afbf3c904c5053f7
Kendra Osburn <kdosburn@gmail.com>
2013-07-24 14:14:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,34 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# ToplayApp::Application.config.secret_token = '86faf202c0f02aa8ad6ad5c91f4085781bc003cd28126e1e40f6d3eaaa1733cbacdf37931c892f9c4ea0f66e8571beb4f60401b8f0289487964b0d64f0087773'
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+ToplayApp::Application.config.secret_key_base = secure_token
