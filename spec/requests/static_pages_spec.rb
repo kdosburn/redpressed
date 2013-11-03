08cd205798d2cb6219fb343d4187776461d895f5
Kendra Osburn <kdosburn@gmail.com>
2013-11-03 15:39:04 -0800

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  subject { page }
 	
 	describe "Home page" do
     subject { page }
@@ -11,6 +13,21 @@ describe "StaticPages" do
     it { should have_title('KO | Home') }
     it { should_not have_title('Pippin') }
 
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
     # it "should have the content 'Home'" do
     # 	#this (above) is the spec
     #   # visit root_path <-- this is commented out because of the before filter
